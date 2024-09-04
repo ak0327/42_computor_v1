631492aa4e36bec47fc31349f0f9940364b374ca
takira <ca.fie.0220@gmail.com>
2024-09-04 15:30:03 +0900

    update(Parser): add member func of Parser class

--- srcs/Parser/Parser.cpp
+++ srcs/Parser/Parser.cpp
@@ -1,1 +1,25 @@
 #include "Parser.hpp"
+
+Parser::Parser() {}
+
+Parser::~Parser() {}
+
+Status Parser::parse_equation(const std::string &equation) noexcept(true) {
+    (void)equation;
+
+    // 左辺、右辺
+
+    // 移項
+
+    return Status::SUCCESS;
+}
+
+Status Parser::validate_equation(int degree) const noexcept(true) {
+    (void)degree;
+
+    return Status::SUCCESS;
+}
+
+std::map<int, int> Parser::get_polynomial() const noexcept(true) {
+    return this->polynomial_;
+}
--- srcs/Parser/Parser.hpp
+++ srcs/Parser/Parser.hpp
@@ -1,5 +1,20 @@
 #pragma once
 
+#include "computor.hpp"
+#include <string>
+#include <map>
+
 class Parser {
+ public:
+    Parser();
+    ~Parser();
+
+    Status parse_equation(const std::string &equation) noexcept(true);
+    Status validate_equation(int degree = 3) const noexcept(true);
+
+    std::map<int, int> get_polynomial() const noexcept(true);
+
+ private:
+    std::map<int, int> polynomial_;
 
 };
