cc846119ac59589c33066d3a942cf125b67b99b5
takira <ca.fie.0220@gmail.com>
2024-09-08 14:08:17 +0900

    add: namespace QuadraticSolver

--- srcs/Calculator/Calculator.cpp
+++ srcs/Calculator/Calculator.cpp
@@ -15,25 +15,25 @@ void Calculator::solve_quadratic_equation() noexcept(true) {
     this->kMaxDegree_ = 2;
 
     // D
-    SolutionType solution_type = Calculator::solve();
+    QuadraticSolver::SolutionType solution_type = Calculator::solve();
     Calculator::display_solution_type(solution_type);
 
     // solve
-    std::vector<Solution> solutions = this->solutions_;
+    std::vector<QuadraticSolver::Solution> solutions = this->solutions_;
     Calculator::display_solutions(solutions, solution_type);
 }
 
 // aX^2 + bX^1 + cX^0 = 0
 //  a == 0 -> 一次
-SolutionType Calculator::solve() noexcept(true) {
+QuadraticSolver::SolutionType Calculator::solve() noexcept(true) {
     int polynomial_min_degree = polynomial_.begin()->first;
     int polynomial_max_degree = polynomial_.rbegin()->first;
 
     if (polynomial_min_degree < this->kMinDegree_) {
-        return NoSolutionDegreeTooLow;
+        return QuadraticSolver::NoSolutionDegreeTooLow;
     }
     if (this->kMaxDegree_ < polynomial_max_degree) {
-        return NoSolutionDegreeTooHigh;
+        return QuadraticSolver::NoSolutionDegreeTooHigh;
     }
 
     try {
@@ -42,43 +42,43 @@ SolutionType Calculator::solve() noexcept(true) {
         b = this->polynomial_.at(1);
         c = this->polynomial_.at(0);
 
-        EquationType eq_type = Calculator::get_equation_type(a, b);
-        SolutionType solution_type;
-        std::vector<Solution> solutions;
+        QuadraticSolver::EquationType eq_type = Calculator::get_equation_type(a, b);
+        QuadraticSolver::SolutionType solution_type;
+        std::vector<QuadraticSolver::Solution> solutions;
         switch (eq_type) {
-            case Quadratic: {
+            case QuadraticSolver::Quadratic: {
                 double D = b * b - 4 * a * c;
                 // std::cout << "D:" << D << std::endl;
                 solution_type = Calculator::get_quadratic_eq_solution_type(D);
                 this->solutions_ = Calculator::solve_quadratic(a, b, D, solution_type);
                 break;
             }
-            case Linear:
-                solution_type = OneRealSolutionLinear;
+            case QuadraticSolver::Linear:
+                solution_type = QuadraticSolver::OneRealSolutionLinear;
                 this->solutions_ = Calculator::solve_linear(b, c, solution_type);
                 break;
-            case Constant:
+            case QuadraticSolver::Constant:
                 solution_type = Calculator::get_constant_eq_solution_type(c);
                 break;
         }
         return solution_type;
     } catch (const std::exception &e) {
         // a, b, c not found
-        return NoSolution;
+        return QuadraticSolver::NoSolution;
     }
 }
 
-std::vector<Solution> Calculator::solve_quadratic(
+std::vector<QuadraticSolver::Solution> Calculator::solve_quadratic(
         double a,
         double b,
         double D,
-        SolutionType type) noexcept(true) {
-    std::vector<Solution> solutions;
+        QuadraticSolver::SolutionType type) noexcept(true) {
+    std::vector<QuadraticSolver::Solution> solutions;
 
     switch (type) {
-        case TwoComplexSolutionsQuadratic: {
+        case QuadraticSolver::TwoComplexSolutionsQuadratic: {
             if (DEBUG) std::cout << "solve_quadratic() 2-complex" << std::endl;
-            Solution ans1, ans2;
+            QuadraticSolver::Solution ans1, ans2;
             ans1 = {
                     .re = -b / 2.0 / a,
                     .im = std::sqrt(-D) / 2.0 / a
@@ -91,18 +91,18 @@ std::vector<Solution> Calculator::solve_quadratic(
             solutions.push_back(ans2);
             break;
         }
-        case TwoRealSolutionsQuadratic: {
+        case QuadraticSolver::TwoRealSolutionsQuadratic: {
             if (DEBUG) std::cout << "solve_quadratic() 2-real" << std::endl;
-            Solution ans1, ans2;
+            QuadraticSolver::Solution ans1, ans2;
             ans1.re = (-b + std::sqrt(D)) / 2.0 / a;
             ans2.re = (-b - std::sqrt(D)) / 2.0 / a;
             solutions.push_back(ans1);
             solutions.push_back(ans2);
             break;
         }
-        case OneRealSolutionQuadratic: {
+        case QuadraticSolver::OneRealSolutionQuadratic: {
             if (DEBUG) std::cout << "solve_quadratic() 1-real" << std::endl;
-            Solution ans;
+            QuadraticSolver::Solution ans;
             ans.re = -b / 2.0 / a;
             solutions.push_back(ans);
             break;
@@ -113,16 +113,16 @@ std::vector<Solution> Calculator::solve_quadratic(
     return solutions;
 }
 
-std::vector<Solution> Calculator::solve_linear(
+std::vector<QuadraticSolver::Solution> Calculator::solve_linear(
         double b,
         double c,
-        SolutionType type) noexcept(true) {
-    std::vector<Solution> solutions;
+        QuadraticSolver::SolutionType type) noexcept(true) {
+    std::vector<QuadraticSolver::Solution> solutions;
 
     switch (type) {
-        case OneRealSolutionLinear: {
+        case QuadraticSolver::OneRealSolutionLinear: {
             if (DEBUG) std::cout << "solve_linear() 1-real" << std::endl;
-            Solution ans;
+            QuadraticSolver::Solution ans;
             ans.re = -c / b;
             solutions.push_back(ans);
             break;
@@ -133,54 +133,54 @@ std::vector<Solution> Calculator::solve_linear(
     return solutions;
 }
 
-EquationType Calculator::get_equation_type(double a, double b) noexcept(true) {
+QuadraticSolver::EquationType Calculator::get_equation_type(double a, double b) noexcept(true) {
     // 2次方程式: aX^2 + bX + c = 0
-    if (a != 0.0) { return Quadratic; }
+    if (a != 0.0) { return QuadraticSolver::Quadratic; }
     // 1次方程式: bX + c = 0
-    if (b != 0.0) { return Linear; }
+    if (b != 0.0) { return QuadraticSolver::Linear; }
     // 0次方程式: c = 0
-    return Constant;
+    return QuadraticSolver::Constant;
 }
 
-SolutionType Calculator::get_quadratic_eq_solution_type(double D) noexcept(true) {
-    if (std::isnan(D) || std::isinf(D)) { return NoSolution; }
-    if (D < 0.0) { return TwoComplexSolutionsQuadratic; }
-    if (0.0 < D) { return TwoRealSolutionsQuadratic; }
-    return OneRealSolutionQuadratic;
+QuadraticSolver::SolutionType Calculator::get_quadratic_eq_solution_type(double D) noexcept(true) {
+    if (std::isnan(D) || std::isinf(D)) { return QuadraticSolver::NoSolution; }
+    if (D < 0.0) { return QuadraticSolver::TwoComplexSolutionsQuadratic; }
+    if (0.0 < D) { return QuadraticSolver::TwoRealSolutionsQuadratic; }
+    return QuadraticSolver::OneRealSolutionQuadratic;
 }
 
-SolutionType Calculator::get_constant_eq_solution_type(double c) noexcept(true) {
+QuadraticSolver::SolutionType Calculator::get_constant_eq_solution_type(double c) noexcept(true) {
     // c = 0 (解なし) or 0 = 0 (不定形)
-    return c != 0.0 ? NoSolution : Indeterminate;
+    return c != 0.0 ? QuadraticSolver::NoSolution : QuadraticSolver::Indeterminate;
 }
 
-void Calculator::display_solution_type(SolutionType type) noexcept(true) {
+void Calculator::display_solution_type(QuadraticSolver::SolutionType type) noexcept(true) {
     std::string solution;
     if (DEBUG) std::cout << get_solution_type(type) << std::endl;
 
     switch (type) {
-        case TwoRealSolutionsQuadratic:
+        case QuadraticSolver::TwoRealSolutionsQuadratic:
             solution = "Discriminant is positive, the two solutions are:";
             break;
-        case TwoComplexSolutionsQuadratic:
+        case QuadraticSolver::TwoComplexSolutionsQuadratic:
             solution = "Discriminant is negative, the two solutions are:";
             break;
-        case OneRealSolutionQuadratic:
+        case QuadraticSolver::OneRealSolutionQuadratic:
             solution = "Discriminant is zero, the solution is:";
             break;
-        case OneRealSolutionLinear:
+        case QuadraticSolver::OneRealSolutionLinear:
             solution = "The solution is:";
             break;
-        case Indeterminate:
+        case QuadraticSolver::Indeterminate:
             solution = "The equation is indeterminate, infinite solutions.";
             break;
-        case NoSolutionDegreeTooLow:
+        case QuadraticSolver::NoSolutionDegreeTooLow:
             solution = "The polynomial degree is strictly less than 0, I can't solve.";
             break;
-        case NoSolutionDegreeTooHigh:
+        case QuadraticSolver::NoSolutionDegreeTooHigh:
             solution = "The polynomial degree is strictly greater than 2, I can't solve.";
             break;
-        case NoSolution:
+        case QuadraticSolver::NoSolution:
             solution = "I can't solve.";
             break;
     }
@@ -188,14 +188,14 @@ void Calculator::display_solution_type(SolutionType type) noexcept(true) {
 }
 
 void Calculator::display_solutions(
-        const std::vector<Solution> &solutions,
-        SolutionType type) noexcept(true) {
+        const std::vector<QuadraticSolver::Solution> &solutions,
+        QuadraticSolver::SolutionType type) noexcept(true) {
     for (const auto &solution : solutions) {
         if (0 < solution.re) {
             std::cout << " ";
         }
         std::cout << std::fixed << std::setprecision(2) << solution.re;
-        if (type == TwoComplexSolutionsQuadratic) {
+        if (type == QuadraticSolver::TwoComplexSolutionsQuadratic) {
             if (0 < solution.im) {
                 std::cout << "+";
             }
@@ -205,23 +205,23 @@ void Calculator::display_solutions(
     }
 }
 
-std::string get_solution_type(SolutionType type) noexcept(true) {
+std::string get_solution_type(QuadraticSolver::SolutionType type) noexcept(true) {
     switch (type) {
-        case TwoRealSolutionsQuadratic:
+        case QuadraticSolver::TwoRealSolutionsQuadratic:
             return "Quadratic: 2-real";
-        case TwoComplexSolutionsQuadratic:
+        case QuadraticSolver::TwoComplexSolutionsQuadratic:
             return "Quadratic: 2-complex";
-        case OneRealSolutionQuadratic:
+        case QuadraticSolver::OneRealSolutionQuadratic:
             return "Quadratic: 1-real";
-        case OneRealSolutionLinear:
+        case QuadraticSolver::OneRealSolutionLinear:
             return "Linear: 1-real";
-        case Indeterminate:
+        case QuadraticSolver::Indeterminate:
             return "Constant: Indeterminate";
-        case NoSolutionDegreeTooLow:
+        case QuadraticSolver::NoSolutionDegreeTooLow:
             return "NoSolution: degree too low";
-        case NoSolutionDegreeTooHigh:
+        case QuadraticSolver::NoSolutionDegreeTooHigh:
             return "NoSolution: degree too high";
-        case NoSolution:
+        case QuadraticSolver::NoSolution:
             return "NoSolution";
         default:
             return "Error";
