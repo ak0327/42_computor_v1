ef97ebd448f8e65131084dbf55b96cdcd966581e
takira <ca.fie.0220@gmail.com>
2024-09-12 08:25:50 +0900

    update: use constexpr

--- srcs/computor.hpp
+++ srcs/computor.hpp
@@ -11,12 +11,12 @@ enum Status {
     FAILURE = 0,
 };
 
-extern const char SP;        // ' '
-extern const char OP_PLUS;   // '+'
-extern const char OP_MINUS;  // '-'
-extern const char OP_MUL;    // '*'
-extern const char OP_EQUAL;  // '='
-extern const char OP_POW;    // '^'
+constexpr char SP       = ' ';
+constexpr char OP_PLUS  = '+';
+constexpr char OP_MINUS = '-';
+constexpr char OP_MUL   = '*';
+constexpr char OP_EQUAL = '=';
+constexpr char OP_POW   = '^';
 
 int calc_equation(const std::string &equation) noexcept(true);
 double normalize_zero(double value) noexcept(true);
